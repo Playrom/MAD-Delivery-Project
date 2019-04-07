54242216f3091aadaf37855f3c5e81b4fbe56bc7
Playrom <Playrom@users.noreply.github.com>
2019-04-07 20:47:40 +0200

    Working Lab2, with some missing parts

--- justorder_framework/src/main/java/it/polito/justorder_framework/Utils.java
+++ justorder_framework/src/main/java/it/polito/justorder_framework/Utils.java
@@ -1,10 +1,17 @@
 package it.polito.justorder_framework;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Utils {
 
@@ -48,5 +55,36 @@ public class Utils {
         }
 
         return hourTime + ":" + minuteTime;
+    }
+
+    public static String createImageFromBitmap(Bitmap bitmap, Context context) {
+        String fileName = "myImage";//no .png or .jpg needed
+        try {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            FileOutputStream fo = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            fo.write(bytes.toByteArray());
+            // remember close file output
+            fo.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fileName = null;
+        }
+        return fileName;
+    }
+
+    public static File createImageFile(Context context) throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Camera");
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                context.getFilesDir()/* directory */
+        );
+
+        return image;
     }
 }
