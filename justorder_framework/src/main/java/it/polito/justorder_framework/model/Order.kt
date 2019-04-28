ecdf7fee7a07b25beee4d2eed3966150554f6ed6
Playrom <Playrom@users.noreply.github.com>
2019-04-28 12:51:03 +0200

    add user and resturant infos on order

--- justorder_framework/src/main/java/it/polito/justorder_framework/model/Order.kt
+++ justorder_framework/src/main/java/it/polito/justorder_framework/model/Order.kt
@@ -5,10 +5,13 @@ import kotlin.collections.HashMap
 
 class Order: Model(){
     var user: String = ""
-    var address: String = ""
+    var userName: String = ""
+    var userAddress: String = ""
     var price: Double = 0.0
     var timestamp: Date = Date()
     var restaurant: String = ""
+    var restaurantName: String = ""
+    var restaurantAddress: String = ""
     var deliverer: String? = null
     var products : Map<String, Int> = mutableMapOf<String, Int>()
 }
