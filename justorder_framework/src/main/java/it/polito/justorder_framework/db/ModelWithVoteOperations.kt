0839bf14530566c6c28729b8e8a795995ddc8afb
Playrom <Playrom@users.noreply.github.com>
2019-05-25 14:53:12 +0200

    fix order

--- justorder_framework/src/main/java/it/polito/justorder_framework/db/ModelWithVoteOperations.kt
+++ justorder_framework/src/main/java/it/polito/justorder_framework/db/ModelWithVoteOperations.kt
@@ -25,4 +25,22 @@ class ModelWithVoteOperations<T: ModelWithVote>(tClass: Class<T>, path: String):
             }
         })
     }
+
+    fun orderByVote(key: String, value: String, cb : (List<T>) -> Unit){
+        var ref = Database.db.child(path)
+        var query = ref.orderByChild(key).equalTo(value).orderByChild("averageVote")
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                var items: MutableList<T> = mutableListOf()
+                for (item in p0.children) {
+                    items.add(Utils.convertObject(item, tClass))
+                }
+                cb(items)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }
 }
